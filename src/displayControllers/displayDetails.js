de95a14a736c52e51a19aca936776a02ba33c797
Dan Snow <danjsnow@outlook.com>
2023-09-15 11:00:02 +0100

    Formatted details box

--- src/displayControllers/displayDetails.js
+++ src/displayControllers/displayDetails.js
@@ -7,6 +7,7 @@ export default function displayDetails(todoItem, detailsContainer) {
     detailsContainer.appendChild(detailsBox);
     detailsBox.classList.add('detailsBox');
   }
+  detailsBox.classList.remove("hidden");
   
   const header = document.createElement('div');
   header.classList.add('detailsBoxHeader');
@@ -18,7 +19,10 @@ export default function displayDetails(todoItem, detailsContainer) {
   
   const closeButton = document.createElement('button');
   closeButton.textContent = 'X';
-  closeButton.onclick = () => detailsBox.innerHTML = null;
+  closeButton.onclick = () => {
+    detailsBox.innerHTML = null;
+    detailsBox.classList.add("hidden");
+  }
   closeButton.classList.add('closeButton');
   header.appendChild(closeButton);
   
