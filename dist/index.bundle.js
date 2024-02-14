2e85987d1f00e6c522105b06c42eeaf948f64c03
Dan Snow <danjsnow@outlook.com>
2024-02-14 14:27:01 +0000

    Adds dist so it shows on gh-pages

---
+++ dist/index.bundle.js
@@ -0,0 +1,24 @@
+(self["webpackChunktodo_app"] = self["webpackChunktodo_app"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+const hello = document.createElement('h1');
+hello.classList.add('hello');
+document.body.appendChild(hello);
+hello.textContent = 'Hello world.';
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvX2FwcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBoZWxsbyA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2gxJyk7XG5oZWxsby5jbGFzc0xpc3QuYWRkKCdoZWxsbycpO1xuZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChoZWxsbyk7XG5oZWxsby50ZXh0Q29udGVudCA9ICdIZWxsbyB3b3JsZC4nO1xuXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
